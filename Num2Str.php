9b7cbd2d75e4c67bc1759c43e982f4d231beea26
Sergey S <5818959@gmail.com>
2019-09-18 18:22:06 +0300

    Adds ability to format not only money but any number.

--- Num2Str.php
+++ Num2Str.php
@@ -4,7 +4,24 @@ namespace nikserg\Num2Str;
 
 class Num2Str
 {
-    public static function convert($num)
+    /**
+     * Формат числа.
+     */
+    const FORMAT_COMMON = 1;
+    /**
+     * Формат суммы в рублях.
+     */
+    const FORMAT_MONEY = 2;
+
+    /**
+     * Возвращает сумму прописью.
+     *
+     * @param float $num
+     * @param int $format
+     * @return string
+     * @author runcore
+     */
+    public static function convert($num, $format = self::FORMAT_MONEY)
     {
         $nul = 'ноль';
         $ten = [
@@ -45,14 +62,9 @@ class Num2Str
             'восемьсот',
             'девятьсот',
         ];
-        $unit = [ // Units
-            ['копейка', 'копейки', 'копеек', 1],
-            ['рубль', 'рубля', 'рублей', 0],
-            ['тысяча', 'тысячи', 'тысяч', 1],
-            ['миллион', 'миллиона', 'миллионов', 0],
-            ['миллиард', 'милиарда', 'миллиардов', 0],
-        ];
-        //
+
+        $unit = self::getUnitsByFormat($format);
+
         list($rub, $kop) = explode('.', sprintf("%015.2f", floatval($num)));
         $out = [];
         if (intval($rub) > 0) {
@@ -81,12 +93,71 @@ class Num2Str
         }
         $out[] = self::morph(intval($rub), $unit[1][0], $unit[1][1], $unit[1][2]); // rub
         $out[] = $kop . ' ' . self::morph($kop, $unit[0][0], $unit[0][1], $unit[0][2]); // kop
+
         return trim(preg_replace('/ {2,}/', ' ', join(' ', $out)));
     }
 
     /**
-     * Склоняем словоформу
-     * @ author runcore
+     * Возращает число прописью.
+     *
+     * @param float $num
+     * @return string
+     * @author Sergey S. <5818959@gmail.com>
+     */
+    public static function common($num)
+    {
+        return self::convert($num, self::FORMAT_COMMON);
+    }
+
+    /**
+     * Возвращает сумму в рублях прописью.
+     *
+     * @param float $num
+     * @return string
+     * @author Sergey S. <5818959@gmail.com>
+     */
+    public static function money($num)
+    {
+        return self::convert($num, self::FORMAT_MONEY);
+    }
+
+    /**
+     * Возвращает склонение единиц измерения в зависимости от формата.
+     *
+     * @param int $format
+     * @return array
+     * @author Sergey S. <5818959@gmail.com>
+     */
+    private static function getUnitsByFormat($format)
+    {
+        if (self::FORMAT_COMMON === $format) {
+            return [
+                ['сотая', 'сотых', 'сотых', 1],
+                ['целая', 'целых', 'целых', 1],
+                ['тысяча', 'тысячи', 'тысяч', 1],
+                ['миллион', 'миллиона', 'миллионов', 0],
+                ['миллиард', 'милиарда', 'миллиардов', 0],
+            ];
+        }
+
+        return [ // Units
+            ['копейка', 'копейки', 'копеек', 1],
+            ['рубль', 'рубля', 'рублей', 0],
+            ['тысяча', 'тысячи', 'тысяч', 1],
+            ['миллион', 'миллиона', 'миллионов', 0],
+            ['миллиард', 'милиарда', 'миллиардов', 0],
+        ];
+    }
+
+    /**
+     * Склоняем словоформу.
+     *
+     * @param int $n
+     * @param string $f1
+     * @param string $f2
+     * @param string $f5
+     * @return string
+     * @author runcore
      */
     private static function morph($n, $f1, $f2, $f5)
     {
